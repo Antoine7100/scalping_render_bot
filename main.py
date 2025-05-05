dff480f056f5ee9d5789c6d5ad623308ce2230aa
Antoine7100 <antoine.therasse91@gmail.com>
2025-05-05 13:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 api_key = os.getenv("BYBIT_API_KEY")
 api_secret = os.getenv("BYBIT_API_SECRET")
-TELEGRAM_BOT_TOKEN = "7962738343:AAGhW_olUPQUFsevN86LNU4VZYvm4hJW5w"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = "1440739670"
 TELEGRAM_USER_ID = 1440739670
 
