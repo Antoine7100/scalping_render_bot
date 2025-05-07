90886f389d618ed725f739a23377806eab7e9e4b
Antoine7100 <antoine.therasse91@gmail.com>
2025-05-08 00:40:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,27 +56,21 @@ async def start_telegram_bot():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     await application.initialize()
 
-    # Définir l'URL du webhook
     webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/bot{TELEGRAM_BOT_TOKEN}"
     await application.bot.set_webhook(url=webhook_url)
 
     application.add_handler(CommandHandler("start", lambda update, context: update.message.reply_text("Bot actif!")))
     application.add_handler(CommandHandler("stop", lambda update, context: update.message.reply_text("Bot arrêté.")))
-    application.add_handler(CommandHandler("status", lambda update, context: update.message.reply_text("Le bot est actif." if bot_running else "Le bot est arrêté.")))
 
-    # Utiliser un port distinct pour le webhook
     await application.start()
     await application.updater.start_webhook(
         listen="0.0.0.0",
-        port=8443,  # Port dédié au webhook
+        port=8443,
         url_path=f"/bot{TELEGRAM_BOT_TOKEN}",
         webhook_url=webhook_url
     )
     print("Bot Telegram démarré avec webhook sur le port 8443")
     await application.idle()
-
-
-
 
 
 async def run_server():
