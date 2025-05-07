4eeec5d0b80e1192089c21b6b87878100c9ec97b
Antoine7100 <antoine.therasse91@gmail.com>
2025-05-08 00:45:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Démarrage du serveur NGINX"
-nginx -c /path/to/nginx.conf
-
-echo "Démarrage du serveur Flask avec Gunicorn"
-gunicorn --bind 0.0.0.0:5000 main:app --workers 4 --threads 2
+echo "Démarrage du serveur Flask avec Waitress"
+waitress-serve --listen=0.0.0.0:10000 main:app
