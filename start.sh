babb742e407ad76d6e7a8bbae462e6f3c233b214
Antoine7100 <antoine.therasse91@gmail.com>
2025-05-08 00:39:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Démarrage du serveur NGINX"
+nginx -c /path/to/nginx.conf
+
 echo "Démarrage du serveur Flask avec Gunicorn"
-gunicorn --bind 0.0.0.0:10000 main:app --workers 4 --threads 2
+gunicorn --bind 0.0.0.0:5000 main:app --workers 4 --threads 2
